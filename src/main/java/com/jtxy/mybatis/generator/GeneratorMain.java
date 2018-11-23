616aa0c0c1b0a6aedb067351fd62d2262c2c0441
yming0000 <33062131+yming0000@users.noreply.github.com>
2018-11-23 17:07:34 +0800

    Add files via upload

---
+++ src/main/java/com/jtxy/mybatis/generator/GeneratorMain.java
@@ -0,0 +1,31 @@
+package com.jtxy.mybatis.generator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GeneratorMain {
+
+	public static void main(String[] args) throws Exception {
+		generator();
+	}
+
+	private static void generator() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("src/main/resources/generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+		System.out.println(">>>>>>>>>>>>>>>>>>");
+	}
+
+}
---
+++ src/main/java/com/jtxy/mybatis/generator/MyJavaTypeResolver.java
@@ -0,0 +1,24 @@
+package com.jtxy.mybatis.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
+
+public class MyJavaTypeResolver extends JavaTypeResolverDefaultImpl {
+
+	@Override
+	protected FullyQualifiedJavaType calculateBigDecimalReplacement(
+			IntrospectedColumn column, FullyQualifiedJavaType defaultType) {
+		FullyQualifiedJavaType answer;
+		if (column.getScale() > 0 || column.getLength() > 18
+				|| forceBigDecimals) {
+			answer = defaultType;
+		} else if (column.getLength() > 9) {
+			answer = new FullyQualifiedJavaType(Long.class.getName());
+		} else {
+			answer = new FullyQualifiedJavaType(Integer.class.getName());
+		}
+		return answer;
+	}
+
+}
